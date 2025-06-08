a54c4cf4bef0c86e35a71d675ade151f0eba602a
sangsooLee <ss_127@naver.com>
2025-06-08 22:10:28 +0900

    Initial Commit

---
+++ Weatherrrr/WeatherrrrApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherrrrApp.swift
+//  Weatherrrr
+//
+//  Created by 이상수 on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherrrrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
